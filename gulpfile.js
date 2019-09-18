625440d569d86f7792b347706ff178e359b5471b
bharatpatodi <bharatpatodi@gmail.com>
2019-09-18 23:39:45 +0530

    Final setup: included browsersync and heroku. Now everything is done for the first time.

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@ const autoprefixer = require('gulp-autoprefixer');
 const eslint = require('gulp-eslint');
 const minifyCSS = require('gulp-clean-css');
 const minifyJS = require('gulp-minify');
+const browserSync = require('browser-sync').create();
 
 // CSS Fixing
 
 function cssFixing() {
-    return gulp.src('styles.css').pipe(autoprefixer()).pipe((minifyCSS())).pipe(gulp.dest('src/styles'));
+    return gulp.src('styles.css').pipe(autoprefixer()).pipe((minifyCSS())).pipe(gulp.dest('src/styles')).pipe(browserSync.stream());
 };
 
 function automatedCSSFixing() {
@@ -17,11 +18,22 @@ function automatedCSSFixing() {
 // JS Fixing
 
 function jsFixing () {
-    return gulp.src('app.js').pipe(eslint()).pipe(minifyJS()).pipe(gulp.dest('src/js'));
+    return gulp.src('app.js').pipe(eslint()).pipe(minifyJS()).pipe(gulp.dest('src/js')).pipe(browserSync.stream());
 }
 
 function automatedJSFixing() {
     return gulp.watch('app.js', jsFixing);
+}
+
+// Browsersync
+function browser_sync() {
+    browserSync.init({
+        // server: {
+        //     baseDir: "./"
+        // },
+        proxy: "localhost:8000" // makes a proxy for the localhost
+    });
+    gulp.watch("*.html").on("change", browserSync.reload);
 }
 
 // Exports
@@ -31,4 +43,4 @@ exports.automatedCSSFixing = automatedCSSFixing;
 exports.jsFixing = jsFixing;
 exports.automatedJSFixing = automatedJSFixing;
 // Default task
-exports.default = gulp.parallel(automatedCSSFixing, automatedJSFixing);
+exports.default = gulp.parallel(automatedCSSFixing, automatedJSFixing, browser_sync);
